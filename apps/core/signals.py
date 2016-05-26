375cebd0a08997cd639b35b162f8aa665014f7a0
jtaghiyar <jtaghiyar@bccrc.ca>
2016-05-26 15:40:37 -0700

    updated.

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -8,11 +8,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Sample, CellTable
 
-# @receiver(post_save, sender=CellTable)
-# def update_num_libraries(sender, instance, **kwargs):
-#     print '>' * 40, instance
-#     import time
-#     time.sleep(20)
-#     sample = instance.sample
-#     sample.num_libraries = len(instance.cell_set.all())
-#     sample.save()
+## Update num_libraries of the Sample instance
+@receiver(post_save, sender=CellTable)
+def update_num_libraries(sender, instance, **kwargs):
+    print '>' * 40, instance
+    sample = instance.sample
+    sample.num_libraries = len(instance.cell_set.all())
+    sample.save()
