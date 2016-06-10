415a7e27164188fd80e71e085f48956916a35868
jtaghiyar <jtaghiyar@bccrc.ca>
2016-06-09 17:56:57 -0700

    minor bug fix.

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -4,12 +4,30 @@ Created on May 24, 2016
 @author: Jafar Taghiyar (jtaghiyar@bccrc.ca)
 """
 
+#===========================
+# Django imports
+#---------------------------
 from django.forms import ModelForm, Form, FileField
 from django.forms import inlineformset_factory
-from .models import Sample, AdditionalSampleInformation
-from .models import Library, SublibraryInformation, LibrarySampleDetail
-from .models import LibraryConstructionInformation, LibraryQuantificationAndStorage
-from .models import Sequencing, SequencingDetail
+
+#===========================
+# App imports
+#---------------------------
+from .models import (
+    Sample,
+    AdditionalSampleInformation,
+    Library,
+    SublibraryInformation,
+    LibrarySampleDetail,
+    LibraryConstructionInformation,
+    LibraryQuantificationAndStorage,
+    Sequencing,
+    SequencingDetail
+    )
+
+#===========================
+# 3rd-party app imports
+#---------------------------
 from taggit.models import Tag
 
 
@@ -21,7 +39,11 @@ class SampleForm(ModelForm):
         model = Sample
         fields = "__all__"
         # widgets = {
-        #     'collect_date': DateWidget(attrs = {'id':'id_dateTimeField'}, bootstrap_version=3, usel10n=True)
+        #     'collect_date': DateWidget(
+        #         attrs = {'id':'id_dateTimeField'},
+        #         bootstrap_version=3,
+        #         usel10n=True
+        #         )
         #     }
         help_texts = {
         'sample_id': ('Sequencing ID (usually SA ID).'),
@@ -32,11 +54,13 @@ class SampleForm(ModelForm):
         }
 
     def clean(self):
-        cleaned_data = super(SampleForm, self).clean()
-        sample_id = cleaned_data.get("sample_id")
-        if len(Sample.objects.filter(sample_id=sample_id)):
-            msg = "Sample ID already exists."
-            self.add_error('sample_id', msg)
+        ## if it's a new instance, the sample_id should not exist.
+        if not self.instance.pk:
+            cleaned_data = super(SampleForm, self).clean()
+            sample_id = cleaned_data.get("sample_id")
+            if len(Sample.objects.filter(sample_id=sample_id)):
+                msg = "Sample ID already exists."
+                self.add_error('sample_id', msg)
 
 AdditionalSampleInfoInlineFormset =  inlineformset_factory(
     Sample,
@@ -54,19 +78,21 @@ class LibraryForm(ModelForm):
     class Meta:
         model = Library
         fields = "__all__"
-        # exclude = ['sample']
+        # exclude = ['projects']
 
     def clean(self):
-        cleaned_data = super(LibraryForm, self).clean()
-        pool_id = cleaned_data.get("pool_id")
-        if len(Library.objects.filter(pool_id=pool_id)):
-            msg = "Pool ID already exists."
-            self.add_error('pool_id', msg)
+        ## if it's a new instance, the pool_id should not exist.
+        if not self.instance.pk:
+            cleaned_data = super(LibraryForm, self).clean()
+            pool_id = cleaned_data.get("pool_id")
+            if len(Library.objects.filter(pool_id=pool_id)):
+                msg = "Pool ID already exists."
+                self.add_error('pool_id', msg)
 
 
 class SublibraryForm(Form):
     ## File field
-    smartchipapp_file = FileField()
+    smartchipapp_file = FileField(label="File")
 
 # SublibraryInfoInlineFormset =  inlineformset_factory(
 #     Library,
