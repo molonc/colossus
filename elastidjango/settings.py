e85ab2125bb0c2f16dec71f0a0e137f73291d1c7
jtaghiyar <jtaghiyar@bccrc.ca>
2016-07-19 15:26:56 -0700

    added VERSION to settings.

--- elastidjango/settings.py
+++ elastidjango/settings.py
@@ -16,6 +16,9 @@ Created on May 16, 2016
 import os
 import sys
 
+# Version
+VERSION = 1.0.0
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
