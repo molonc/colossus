8a488fd57cfaf197ce6b90fc6f50019d4d5181cc
Andrew McPherson <amcphers@bccrc.ca>
2018-01-12 13:56:54 -0800

    minor: cell count plot fixes

--- scripts/every_cell_count_plot.R
+++ scripts/every_cell_count_plot.R
@@ -13,11 +13,11 @@ if (is.na(output)) output <- "output.pdf"
 # if (is.na(log)) log <- FALSE
 
 input$submission_date <- as.Date(input$submission_date)
-input <- input[order(input$submission_date), ]
+input <- input[order(input$submission_date, input$pool_id), ]
 
-# filtered <- input[grep("ABC", input$pool_id, invert = TRUE), ]
-filtered <- input
+filtered <- input[grep("ABC", input$pool_id, invert = TRUE), ]
 filtered$cumsum <- cumsum(filtered$count)
+filtered <- filtered[!duplicated(filtered$pool_id), ]
 
 # if (is.na(stop)) stop <- max(filtered$submission_date)
 # stop <- max(filtered$submission_date)
@@ -28,7 +28,8 @@ g <- ggplot(filtered, aes(submission_date, cumsum)) + geom_line() + scale_x_date
 print(g)
 g <- ggplot(filtered, aes(submission_date, cumsum)) + geom_line() + scale_x_date(date_breaks="1 month", "Date") + scale_y_continuous("Total Cells Sequenced") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 print(g)
-g <- ggplot(filtered, aes(pool_id, count)) + geom_col() + scale_y_continuous("Cells Sequenced") + scale_x_discrete("Library (Chronological)") + theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5))
+filtered$pool_id <- factor(filtered$pool_id, levels = filtered$pool_id)
+g <- ggplot(filtered, aes(pool_id, count)) + geom_col() + scale_y_continuous("Cells Sequenced") + scale_x_discrete("Library (Chronological)") + theme(axis.text.x = element_text(angle = 90, hjust = 0, vjust = 0.5, size = 7))
 print(g)
 
 # ggsave(filename = output, width = w, height = h)
