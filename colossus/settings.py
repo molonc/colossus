b0b574174030f517d0678bebad514a4e9568b06c
Matt Wiens <mwiens91@gmail.com>
2018-09-04 14:35:14 -0700

    Allow media/ and static/ for dev servers

--- colossus/settings.py
+++ colossus/settings.py
@@ -145,16 +145,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# STATICDILES_DIR = []
-
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn") 
-STATIC_ROOT = "/var/www/html/singlecell/static/"
+STATIC_ROOT = os.environ.get(
+    'COLOSSUS_STATIC_ROOT',
+    os.path.join(BASE_DIR, 'static/'),)
 
 # Media files
 MEDIA_URL = "/media/"
-#MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "uploads")
-MEDIA_ROOT = "/var/www/html/singlecell/media/"
+MEDIA_ROOT = os.environ.get(
+    'COLOSSUS_MEDIA_ROOT',
+    os.path.join(BASE_DIR, 'media/'),)
 
 # Login url
 LOGIN_URL = '/account/login/'
