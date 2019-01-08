ee661d4784adfaa8941485763621ad29f89ebab0
Simong Song <ssong@momac22.bccrc.ca>
2019-01-08 10:59:17 -0800

    COLO-63/number of lanes requested updated

--- core/views.py
+++ core/views.py
@@ -1095,6 +1095,21 @@ class SequencingDetail(TemplateView):
 
     template_name = "core/sequencing_detail.html"
 
+    def update_number_of_lanes_requested(self):
+        sequences = self.sequencing_class.objects.all()
+        for l in sequences:
+           if l.library_type == 'dlp':
+               if hasattr(l,'dlpsequencingdetail'):
+                   if l.dlpsequencingdetail.number_of_lanes_requested < l.dlplane_set.all().count():
+                       l.dlpsequencingdetail.number_of_lanes_requested=l.dlplane_set.all().count()
+                       l.dlpsequencingdetail.save()
+                   else:
+                       pass
+               else:
+                   pass
+           else:
+               pass
+
     def get(self, request, pk):
         key = "gsc_form_metadata_%s" % pk
         download = False
@@ -1106,6 +1121,9 @@ class SequencingDetail(TemplateView):
             'download': download,
             'library_type': self.library_type,
         }
+
+        self.update_number_of_lanes_requested()
+
         return render(request, self.template_name, context)
 
 
