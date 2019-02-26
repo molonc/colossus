f5243564a62e5d26a30367167018074058b2b4b1
Mike Yue <myue@bccrc.ca>
2019-02-26 15:24:03 -0800

    Changed uniqueness constraint of sequencings for Emma

--- core/models.py
+++ core/models.py
@@ -1046,7 +1046,7 @@ class Sequencing(models.Model, FieldValue):
 
     class Meta:
         abstract = True
-        unique_together = ('library', 'sequencing_center')
+        unique_together = ('library', 'sequencing_center', 'sequencing_instrument')
 
     fields_to_exclude = ['ID', 'Library']
     values_to_exclude = ['id', 'library']
