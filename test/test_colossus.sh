de91f8fc86e0b2577f5b9d678784fd870155502f
Ubuntu <ubuntu@colossus.u2nhbv4qm50etksoe2ddos5hob.ux.internal.cloudapp.net>
2019-03-29 18:38:28 +0000

    Changed pull to fetch + reset, added makemigration check on test server

--- test/test_colossus.sh
+++ test/test_colossus.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -v
+set -e
 cd /home/ubuntu/colossus
-git pull
+git fetch origin
+git reset --hard origin/master
 source venv/bin/activate
 pip3 install -r requirements.txt --ignore-installed
 python3 manage.py migrate
+python3 manage.py makemigrations --check
